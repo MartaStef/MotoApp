9be7779b66d223ee1827dc30d20d93701fb0f102
Marta Stefanska <marta.stefanska@outlook.es>
2024-11-16 19:42:33 +0100

    stos generyczny - wstep

--- MotoApp/MotoApp/Program.cs
+++ MotoApp/MotoApp/Program.cs
@@ -1,2 +1,26 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using MotoApp;
+
+var stack = new BasicStack<double>();
+
+stack.Push(4.6);
+stack.Push(43);
+stack.Push(333.5);
+
+double sum = 0.0;
+
+var stackString = new BasicStack<string>();
+
+stackString.Push("Company1");
+stackString.Push("Company2");
+stackString.Push("Company3");
+
+while (stack.Count > 0)
+{
+    double item = stack.Pop();
+    Console.WriteLine($"item:{item}");
+    sum += item;
+}
+
+Console.WriteLine($"sum:{sum}");
+
+//napisac test jednostkowy
